e480b10508874d1060e6b909d710fc3a28d289c6
Theo Sanderson <theo@sndrsn.co.uk>
2021-06-25 21:36:17 +0100

    GISAID wip
    Former-commit-id: a942cbc7b95c7a3c167d698042bacc2ba021fac4

--- src/components/SearchPanel.jsx
+++ src/components/SearchPanel.jsx
@@ -1,7 +1,14 @@
 import React from "react";
-
+import { FaSearch } from "react-icons/fa";
 function SearchPanel() {
-  return <div>woo</div>;
+  return (
+    <div>
+      <h2 className="text-xl mt-5">
+        <FaSearch className="inline-block mr-2" />
+        Search
+      </h2>
+    </div>
+  );
 }
 
 export default SearchPanel;
