eb8fd7e8951a2b574521e13f152093b85953b8c4
Theo Sanderson <theo@sndrsn.co.uk>
2021-06-28 17:41:11 +0100

    search gui

--- src/components/GISAIDLoader.jsx
+++ src/components/GISAIDLoader.jsx
@@ -1,88 +1,119 @@
-import React, {useRef, useCallback} from "react";
+import React, { useRef, useCallback } from "react";
 
-function GISAIDLoader({setGisaid,enabled,setGisaidLoaderEnabled}) {
+function GISAIDLoader({ setGisaid, enabled, setGisaidLoaderEnabled }) {
+  const GISAID_ID_COLUMN = 0;
+  const GISAID_PANGO_COLUMN = 11;
+  const GISAID_AA_SUBS_COLUMN = 13;
 
-    
+  const fileSelector = useRef();
+  const bar = useRef();
 
-    const fileSelector = useRef();
-    const bar = useRef();
+  const supplyGISAIDdata = useCallback(() => {
+    let lookup = {};
 
-
-    const supplyGISAIDdata = useCallback(() => {
-        let lookup = {}
-    
-      const theFile = fileSelector.current.files[0];
-      let reader = new FileReader();
-      reader.addEventListener("load", function (e) {
-        let text = e.target.result;
-        window.text = text;
-        const lines = text.split("\n");
-        lines.shift();
-        lines.pop();
-        lines.forEach((x) => {
-          const entries = x.split("\t");
-          lookup[entries[0]] = entries[18];
-        });
-        chunk_count += 1;
-
-
-        const prop = 100*chunk_count*chunk_size/theFile.size
-        bar.current.style.width=""+prop+"%"
-  
-        if (chunk_size * chunk_count > theFile.size) {
-          setGisaid(lookup)
-          setGisaidLoaderEnabled(false)
-          return;
-        }
-    
-        e.target.readAsText(
-            theFile.slice(
-            chunk_size * chunk_count,
-            chunk_size * (chunk_count + 1) + 2000
-          )
-        );
-  
-        //console.log(text.byteLength);
-        //const result = pako.inflate(text, { to: "string" });
-        //const out = result;
-        //window.out = out;
-        //console.log("dpme", out.length);
+    const theFile = fileSelector.current.files[0];
+    let reader = new FileReader();
+    reader.addEventListener("load", function (e) {
+      let text = e.target.result;
+      window.text = text;
+      const lines = text.split("\n");
+      lines.shift();
+      lines.pop();
+      lines.forEach((x) => {
+        const entries = x.split("\t");
+        lookup[GISAID_ID_COLUMN] = {
+          lineage: entries[GISAID_PANGO_COLUMN],
+          aa_subs: entries[GISAID_AA_SUBS_COLUMN],
+        };
       });
-      const chunk_size = 1000000;
-      let  chunk_count = 0;
-      reader.readAsText(
+      chunk_count += 1;
+
+      const prop = (100 * chunk_count * chunk_size) / theFile.size;
+      bar.current.style.width = "" + prop + "%";
+
+      if (chunk_size * chunk_count > theFile.size) {
+        setGisaid(lookup);
+        setGisaidLoaderEnabled(false);
+        return;
+      }
+
+      e.target.readAsText(
         theFile.slice(
           chunk_size * chunk_count,
-          chunk_size * chunk_count + 2000
+          chunk_size * (chunk_count + 1) + 2000
         )
       );
-    }, [fileSelector, setGisaidLoaderEnabled, setGisaid]);
 
+      //console.log(text.byteLength);
+      //const result = pako.inflate(text, { to: "string" });
+      //const out = result;
+      //window.out = out;
+      //console.log("dpme", out.length);
+    });
+    const chunk_size = 1000000;
+    let chunk_count = 0;
+    reader.readAsText(
+      theFile.slice(chunk_size * chunk_count, chunk_size * chunk_count + 2000)
+    );
+  }, [fileSelector, setGisaidLoaderEnabled, setGisaid]);
 
-  return (<div className={`${enabled ? "" : "hidden"}`}>
-    <div className="fixed w-full h-full bg-black opacity-80 z-40"> </div>
-    <div className="fixed m-10 p-5 bg-white shadow-md z-50">
-        <button className="absolute top-5 right-5 text-xl font-bold" onClick={()=> setGisaidLoaderEnabled(false)}>X</button>
-      <h1 className="font-bold mb-5 text-xl">Import GISAID metadata</h1>
-      <p>
-          The GISAID database licence does not allow us to redistribute of metadata on sequences contained within it. Therefore, to add GISAID metadata to this visualisation, you will need to download it and then add it yourself. The metadata file will remain on your computer: it will not be uploaded to any server.
-      </p>
-      <p className="mb-5 mt-10">Step 1: If you have not already, <a className="text-blue-800 underline" href="https://www.gisaid.org/registration/register/">Register for a GISAID account</a> and wait for approval.</p>
-      <p className="mb-5">Step 2: Once approved, <a className="text-blue-800 underline"  href="https://www.epicov.org/epi3/frontend#2218a6">sign in</a> and download from: EpiCov -&gt; Downloads -&gt; Genomic epidemiology -&gt; metadata </p>
-      <p className="mb-5">Step 3: Unzip the metadata_yyyy_mm_dd.tsv.gz file</p>
-      <p className="mb-5">Step 4: Select the unzipped metadata.tsv file:  <input
-              type="file"
-              id="files"
-              ref={fileSelector}
-              onChange={supplyGISAIDdata}
-            /></p>
+  return (
+    <div className={`${enabled ? "" : "hidden"}`}>
+      <div className="fixed w-full h-full bg-black opacity-80 z-40"> </div>
+      <div className="fixed m-10 p-5 bg-white shadow-md z-50">
+        <button
+          className="absolute top-5 right-5 text-xl font-bold"
+          onClick={() => setGisaidLoaderEnabled(false)}
+        >
+          X
+        </button>
+        <h1 className="font-bold mb-5 text-xl">Import GISAID metadata</h1>
+        <p>
+          The GISAID database licence does not allow us to redistribute of
+          metadata on sequences contained within it. Therefore, to add GISAID
+          metadata to this visualisation, you will need to download it and then
+          add it yourself. The metadata file will remain on your computer: it
+          will not be uploaded to any server.
+        </p>
+        <p className="mb-5 mt-10">
+          Step 1: If you have not already,{" "}
+          <a
+            className="text-blue-800 underline"
+            href="https://www.gisaid.org/registration/register/"
+          >
+            Register for a GISAID account
+          </a>{" "}
+          and wait for approval.
+        </p>
+        <p className="mb-5">
+          Step 2: Once approved,{" "}
+          <a
+            className="text-blue-800 underline"
+            href="https://www.epicov.org/epi3/frontend#2218a6"
+          >
+            sign in
+          </a>{" "}
+          and download from: EpiCov -&gt; Downloads -&gt; Genomic epidemiology
+          -&gt; metadata{" "}
+        </p>
+        <p className="mb-5">
+          Step 3: Unzip the metadata_yyyy_mm_dd.tsv.gz file
+        </p>
+        <p className="mb-5">
+          Step 4: Select the unzipped metadata.tsv file:{" "}
+          <input
+            type="file"
+            id="files"
+            ref={fileSelector}
+            onChange={supplyGISAIDdata}
+          />
+        </p>
 
-            <div className="mx-auto h-7 w-4/5 bg-gray-200 mt-5">
-
-            <div className=" h-7 bg-green-700 w-0 inline-block" ref={bar}></div>
-            </div>
-      
-    </div></div>
+        <div className="mx-auto h-7 w-4/5 bg-gray-200 mt-5">
+          <div className=" h-7 bg-green-700 w-0 inline-block" ref={bar}></div>
+        </div>
+      </div>
+    </div>
   );
 }
 
--- src/components/SearchPanel.jsx
+++ src/components/SearchPanel.jsx
@@ -1,12 +1,64 @@
 import React from "react";
+import SearchItem from "./SearchItem";
 import { FaSearch } from "react-icons/fa";
-function SearchPanel() {
+import { RiAddCircleLine } from "react-icons/ri";
+import { BiPalette } from "react-icons/bi";
+function SearchPanel({ searchItems, setSearchItems, colourBy, setColourBy }) {
   return (
     <div>
-      <h2 className="text-xl mt-5">
-        <FaSearch className="inline-block mr-2" />
-        Search
-      </h2>
+      <div className="border-b border-gray-300">
+        <h2 className="text-xl mt-5 mb-4 text-gray-700">
+          <FaSearch className="inline-block mr-2" />
+          Search
+        </h2>
+        {searchItems.map(function (item, index) {
+          return (
+            <SearchItem
+              key={item.id}
+              id={item.id}
+              category={item.category}
+              value={item.value}
+              setThis={(mapping) => {
+                searchItems[index] = { ...searchItems[index], ...mapping };
+                setSearchItems([...searchItems]);
+              }}
+              removeItem={(id) => {
+                console.log("remove", id);
+                setSearchItems(searchItems.filter((x) => x.id !== id));
+              }}
+            ></SearchItem>
+          );
+        })}
+
+        <button
+          className="block bg-gray-100 text-sm mx-auto p-1 rounded border-gray-300 border m-5 text-gray-700"
+          onClick={() =>
+            setSearchItems([
+              ...searchItems,
+              { id: Math.random(), category: "name", value: null },
+            ])
+          }
+        >
+          <RiAddCircleLine className="inline-block mr-2" />
+          Add a new search
+        </button>
+      </div>
+      <div>
+        <h2 className="text-xl mt-5 mb-4 text-gray-700">
+          <BiPalette className="inline-block mr-2" />
+          Colour by
+        </h2>
+
+        <select
+          className="border py-2 px-3 text-grey-darkest"
+          value={colourBy}
+          onChange={(event) => setColourBy(event.target.value)}
+        >
+          <option value="lineage">Lineage</option>
+          <option value="country">Country</option>
+          <option value="none">None</option>
+        </select>
+      </div>
     </div>
   );
 }
