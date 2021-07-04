c09f8fc0a85840aea6e761fa9c99f2c7ddffb047
theosanderson <theo@theo.io>
2021-07-04 14:33:31 +0100

    progress

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import "./Spinner.css";
 
-function Spinner({ isShown }) {
+function Spinner({ isShown , progress}) {
   if (isShown) {
     return (
       <div className="w-full h-full fixed bg-white text-center">
         {" "}
         <div className="loader z-50">Loading...</div>
-        <div className="text-black">Loading</div>
+        <div className="text-black">Loading {progress}%</div>
       </div>
     );
   } else {
