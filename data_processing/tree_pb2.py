b98ae01226443a0d43d9a5526faad08ecf0f6bc5
theosanderson <theo@theo.io>
2021-07-04 02:41:54 +0100

    updates
    Former-commit-id: e5beebbccd9ab463af194d1b8cc0d43ad7ffce35

--- data_processing/tree_pb2.py
+++ data_processing/tree_pb2.py
@@ -19,7 +19,7 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   package='',
   syntax='proto3',
   serialized_options=None,
-  serialized_pb=_b('\n\ntree.proto\":\n\x04Node\x12\x0c\n\x04name\x18\x01 \x01(\t\x12\t\n\x01x\x18\x02 \x01(\x02\x12\t\n\x01y\x18\x03 \x01(\x02\x12\x0e\n\x06parent\x18\x04 \x01(\x05\" \n\x08NodeList\x12\x14\n\x05nodes\x18\x01 \x03(\x0b\x32\x05.Node\"]\n\x0cMetadataItem\x12\x0c\n\x04name\x18\x01 \x01(\t\x12\x0f\n\x07lineage\x18\x02 \x01(\t\x12\x0c\n\x04\x64\x61te\x18\x03 \x01(\t\x12\x0f\n\x07\x63ountry\x18\x04 \x01(\t\x12\x0f\n\x07\x61\x61_subs\x18\x05 \x03(\t\",\n\x0cMetadataList\x12\x1c\n\x05items\x18\x01 \x03(\x0b\x32\r.MetadataItemb\x06proto3')
+  serialized_pb=_b('\n\ntree.proto\":\n\x04Node\x12\x0c\n\x04name\x18\x01 \x01(\t\x12\t\n\x01x\x18\x02 \x01(\x02\x12\t\n\x01y\x18\x03 \x01(\x02\x12\x0e\n\x06parent\x18\x04 \x01(\x05\" \n\x08NodeList\x12\x14\n\x05nodes\x18\x01 \x03(\x0b\x32\x05.Node\"]\n\x0cMetadataItem\x12\x0c\n\x04name\x18\x01 \x01(\t\x12\x0f\n\x07lineage\x18\x02 \x01(\t\x12\x0c\n\x04\x64\x61te\x18\x03 \x01(\t\x12\x0f\n\x07\x63ountry\x18\x04 \x01(\t\x12\x0f\n\x07\x61\x61_subs\x18\x05 \x03(\t\",\n\x0cMetadataList\x12\x1c\n\x05items\x18\x01 \x03(\x0b\x32\r.MetadataItem\" \n\x0cMutationList\x12\x10\n\x08mutation\x18\x01 \x03(\x05\"\x99\x01\n\x0b\x41llNodeData\x12\r\n\x05names\x18\x01 \x03(\t\x12\t\n\x01x\x18\x02 \x03(\x02\x12\t\n\x01y\x18\x03 \x03(\x02\x12\x11\n\tcountries\x18\x04 \x03(\x05\x12\x10\n\x08lineages\x18\x05 \x03(\x05\x12 \n\tmutations\x18\x06 \x03(\x0b\x32\r.MutationList\x12\r\n\x05\x64\x61tes\x18\x07 \x03(\x05\x12\x0f\n\x07parents\x18\x08 \x03(\x05\"\x8c\x01\n\x07\x41llData\x12\x1f\n\tnode_data\x18\x01 \x01(\x0b\x32\x0c.AllNodeData\x12\x17\n\x0f\x63ountry_mapping\x18\x02 \x03(\t\x12\x17\n\x0flineage_mapping\x18\x03 \x03(\t\x12\x18\n\x10mutation_mapping\x18\x04 \x03(\t\x12\x14\n\x0c\x64\x61te_mapping\x18\x05 \x03(\tb\x06proto3')
 )
 
 
@@ -197,12 +197,187 @@ _METADATALIST = _descriptor.Descriptor(
   serialized_end=247,
 )
 
+
+_MUTATIONLIST = _descriptor.Descriptor(
+  name='MutationList',
+  full_name='MutationList',
+  filename=None,
+  file=DESCRIPTOR,
+  containing_type=None,
+  fields=[
+    _descriptor.FieldDescriptor(
+      name='mutation', full_name='MutationList.mutation', index=0,
+      number=1, type=5, cpp_type=1, label=3,
+      has_default_value=False, default_value=[],
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR),
+  ],
+  extensions=[
+  ],
+  nested_types=[],
+  enum_types=[
+  ],
+  serialized_options=None,
+  is_extendable=False,
+  syntax='proto3',
+  extension_ranges=[],
+  oneofs=[
+  ],
+  serialized_start=249,
+  serialized_end=281,
+)
+
+
+_ALLNODEDATA = _descriptor.Descriptor(
+  name='AllNodeData',
+  full_name='AllNodeData',
+  filename=None,
+  file=DESCRIPTOR,
+  containing_type=None,
+  fields=[
+    _descriptor.FieldDescriptor(
+      name='names', full_name='AllNodeData.names', index=0,
+      number=1, type=9, cpp_type=9, label=3,
+      has_default_value=False, default_value=[],
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='x', full_name='AllNodeData.x', index=1,
+      number=2, type=2, cpp_type=6, label=3,
+      has_default_value=False, default_value=[],
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='y', full_name='AllNodeData.y', index=2,
+      number=3, type=2, cpp_type=6, label=3,
+      has_default_value=False, default_value=[],
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='countries', full_name='AllNodeData.countries', index=3,
+      number=4, type=5, cpp_type=1, label=3,
+      has_default_value=False, default_value=[],
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='lineages', full_name='AllNodeData.lineages', index=4,
+      number=5, type=5, cpp_type=1, label=3,
+      has_default_value=False, default_value=[],
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='mutations', full_name='AllNodeData.mutations', index=5,
+      number=6, type=11, cpp_type=10, label=3,
+      has_default_value=False, default_value=[],
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='dates', full_name='AllNodeData.dates', index=6,
+      number=7, type=5, cpp_type=1, label=3,
+      has_default_value=False, default_value=[],
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='parents', full_name='AllNodeData.parents', index=7,
+      number=8, type=5, cpp_type=1, label=3,
+      has_default_value=False, default_value=[],
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR),
+  ],
+  extensions=[
+  ],
+  nested_types=[],
+  enum_types=[
+  ],
+  serialized_options=None,
+  is_extendable=False,
+  syntax='proto3',
+  extension_ranges=[],
+  oneofs=[
+  ],
+  serialized_start=284,
+  serialized_end=437,
+)
+
+
+_ALLDATA = _descriptor.Descriptor(
+  name='AllData',
+  full_name='AllData',
+  filename=None,
+  file=DESCRIPTOR,
+  containing_type=None,
+  fields=[
+    _descriptor.FieldDescriptor(
+      name='node_data', full_name='AllData.node_data', index=0,
+      number=1, type=11, cpp_type=10, label=1,
+      has_default_value=False, default_value=None,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='country_mapping', full_name='AllData.country_mapping', index=1,
+      number=2, type=9, cpp_type=9, label=3,
+      has_default_value=False, default_value=[],
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='lineage_mapping', full_name='AllData.lineage_mapping', index=2,
+      number=3, type=9, cpp_type=9, label=3,
+      has_default_value=False, default_value=[],
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='mutation_mapping', full_name='AllData.mutation_mapping', index=3,
+      number=4, type=9, cpp_type=9, label=3,
+      has_default_value=False, default_value=[],
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='date_mapping', full_name='AllData.date_mapping', index=4,
+      number=5, type=9, cpp_type=9, label=3,
+      has_default_value=False, default_value=[],
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR),
+  ],
+  extensions=[
+  ],
+  nested_types=[],
+  enum_types=[
+  ],
+  serialized_options=None,
+  is_extendable=False,
+  syntax='proto3',
+  extension_ranges=[],
+  oneofs=[
+  ],
+  serialized_start=440,
+  serialized_end=580,
+)
+
 _NODELIST.fields_by_name['nodes'].message_type = _NODE
 _METADATALIST.fields_by_name['items'].message_type = _METADATAITEM
+_ALLNODEDATA.fields_by_name['mutations'].message_type = _MUTATIONLIST
+_ALLDATA.fields_by_name['node_data'].message_type = _ALLNODEDATA
 DESCRIPTOR.message_types_by_name['Node'] = _NODE
 DESCRIPTOR.message_types_by_name['NodeList'] = _NODELIST
 DESCRIPTOR.message_types_by_name['MetadataItem'] = _METADATAITEM
 DESCRIPTOR.message_types_by_name['MetadataList'] = _METADATALIST
+DESCRIPTOR.message_types_by_name['MutationList'] = _MUTATIONLIST
+DESCRIPTOR.message_types_by_name['AllNodeData'] = _ALLNODEDATA
+DESCRIPTOR.message_types_by_name['AllData'] = _ALLDATA
 _sym_db.RegisterFileDescriptor(DESCRIPTOR)
 
 Node = _reflection.GeneratedProtocolMessageType('Node', (_message.Message,), dict(
@@ -232,6 +407,27 @@ MetadataList = _reflection.GeneratedProtocolMessageType('MetadataList', (_messag
   # @@protoc_insertion_point(class_scope:MetadataList)
   ))
 _sym_db.RegisterMessage(MetadataList)
+
+MutationList = _reflection.GeneratedProtocolMessageType('MutationList', (_message.Message,), dict(
+  DESCRIPTOR = _MUTATIONLIST,
+  __module__ = 'tree_pb2'
+  # @@protoc_insertion_point(class_scope:MutationList)
+  ))
+_sym_db.RegisterMessage(MutationList)
+
+AllNodeData = _reflection.GeneratedProtocolMessageType('AllNodeData', (_message.Message,), dict(
+  DESCRIPTOR = _ALLNODEDATA,
+  __module__ = 'tree_pb2'
+  # @@protoc_insertion_point(class_scope:AllNodeData)
+  ))
+_sym_db.RegisterMessage(AllNodeData)
+
+AllData = _reflection.GeneratedProtocolMessageType('AllData', (_message.Message,), dict(
+  DESCRIPTOR = _ALLDATA,
+  __module__ = 'tree_pb2'
+  # @@protoc_insertion_point(class_scope:AllData)
+  ))
+_sym_db.RegisterMessage(AllData)
 
 
 # @@protoc_insertion_point(module_scope)
