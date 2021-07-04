9e642db6664e8e6eca656e599a48f9e39a41d8b6
theosanderson <theo@theo.io>
2021-07-04 15:46:57 +0100

    Updates

--- src/components/SearchPanel.jsx
+++ src/components/SearchPanel.jsx
@@ -3,8 +3,11 @@ import SearchItem from "./SearchItem";
 import { FaSearch } from "react-icons/fa";
 import { RiAddCircleLine } from "react-icons/ri";
 import { BiPalette } from "react-icons/bi";
+import { BsInfoCircle } from "react-icons/bs";
 
-function SearchPanel({ searchItems, setSearchItems, colourBy, setColourBy }) {
+
+function SearchPanel({ searchItems, setSearchItems, colourBy, setColourBy ,selectedNode,data}) {
+ const node_data = data.node_data
   return (
     <div>
       <div className="border-b border-gray-300">
@@ -50,7 +53,7 @@ function SearchPanel({ searchItems, setSearchItems, colourBy, setColourBy }) {
           Add a new search
         </button>
       </div>
-      <div>
+      <div className="border-b border-gray-300 pb-3">
         <h2 className="text-xl mt-5 mb-4 text-gray-700">
           <BiPalette className="inline-block mr-2" />
           Colour by
@@ -66,6 +69,33 @@ function SearchPanel({ searchItems, setSearchItems, colourBy, setColourBy }) {
           <option value="none">None</option>
         </select>
       </div>
+      <div>
+        
+        {selectedNode&& <div className="text-gray-500"><h2 className="text-xl mt-5 mb-4 text-gray-700">
+          <BsInfoCircle className="inline-block mr-2" />
+          Node info
+        </h2>
+       
+        <div className="font-bold">
+        {node_data.names[selectedNode]}
+        </div>
+        <div>
+        {data.date_mapping[node_data.dates[selectedNode]]}
+        </div>
+        <div>
+        {data.lineage_mapping[node_data.lineages[selectedNode]]}
+        </div>
+        <div>
+        {data.country_mapping[node_data.countries[selectedNode]]}
+        </div>
+        <div className="text-xs mr-5">
+            {
+              
+              node_data.mutations[selectedNode].mutation&&node_data.mutations[selectedNode].mutation.map(x=>data.mutation_mapping[x]).join(", ") //TODO assign the top thing to a constant and use it again
+            }
+          </div></div>
+          }
+        </div>
     </div>
   );
 }
