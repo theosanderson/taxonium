4db847045e890772d138d46bb397bfeac2d446b4
Theo Sanderson <theo@sndrsn.co.uk>
2021-10-04 14:26:16 +0100

    Update Deck.jsx

--- src/Deck.jsx
+++ src/Deck.jsx
@@ -306,7 +306,7 @@ function Deck({
         return;
       }
 
-      viewState["minimap"] = { zoom: 2.0, target: [5, 16] };
+      viewState["minimap"] = { zoom: 2.4, target: [5, 16] };
       if (zoom_in_x) {
         if (oldViewState) {
           viewState.target[1] = oldViewState.target[1];
