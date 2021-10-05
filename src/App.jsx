fcc4bababaea7263200fabc9575d67b68a264f9a
Theo Sanderson <theo@sndrsn.co.uk>
2021-10-05 12:54:55 +0100

    Update App.jsx
    Former-commit-id: 4abb06eab6df4e38cf60d8d287135d5147f7e200

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
       reader.readAsArrayBuffer(file);
     }
     const [query, setQuery] = useQueryAsState({
-      blinking:"true",
+      blinking:"false",
         search: JSON.stringify([
           {
             id: 0.123,
