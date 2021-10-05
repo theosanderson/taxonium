316f112e9b3659aa105beaf01ca22c47ba1738b2
theosanderson <theo@theo.io>
2021-10-05 22:47:43 +0100

    Remove unused
    Former-commit-id: 4382493f49682f714de1c65e473137a6104226e4

--- src/components/SearchPanel.jsx
+++ src/components/SearchPanel.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from "react";
+import React, {  useMemo, useState } from "react";
 import SearchItem from "./SearchItem";
 import { FaSearch } from "react-icons/fa";
 import { RiAddCircleLine } from "react-icons/ri";
