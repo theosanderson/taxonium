7a4fdd469279073db73aeb4dd5f3709bc641ce36
Theo Sanderson <theo@sndrsn.co.uk>
2022-01-31 15:05:16 +0000

    Update Taxonium.jsx

--- src/Taxonium.jsx
+++ src/Taxonium.jsx
@@ -84,7 +84,9 @@ function Taxonium({ protoUrl, uploadedData, query, setQuery }) {
     if (xType === "time") {
       newData.data.node_data.x_cur = newData.data.node_data.time_x;
     } else {
+      if(newData.data.node_data.x){
       newData.data.node_data.x_cur = newData.data.node_data.x.map( x=> 0.1*x);
+      }
     }
     console.log(xType);
     return newData;
