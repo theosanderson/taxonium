48318a0e917b8c08217c0b787c693599d21d8f02
Theo Sanderson <theo@sndrsn.co.uk>
2022-02-10 00:09:31 +0000

    color by working nicely. bugs refreshing minimap and using cache
    Former-commit-id: 1333c2fff3ec4fe891396e8cade31c149db082c2

--- src/components/SearchPanel.jsx
+++ src/components/SearchPanel.jsx
@@ -1,6 +1,7 @@
 import SearchTopLayerItem from "./SearchTopLayerItem";
 import { RiAddCircleLine } from "react-icons/ri";
 
+const genes = ["S", "N"];
 function SearchPanel({ search, colorBy }) {
   return (
     <div className="overflow-y-auto" style={{ height: "calc(100vh - 5em)" }}>
@@ -32,6 +33,29 @@ function SearchPanel({ search, colorBy }) {
           </option>
         ))}
       </select>
+      {colorBy.colorByField === "genotype" && (
+        <div>
+          Gene:{" "}
+          <select
+            value={colorBy.colorByGene}
+            onChange={(e) => colorBy.setColorByGene(e.target.value)}
+          >
+            {genes.map((item) => (
+              <option key={item} value={item}>
+                {item}
+              </option>
+            ))}
+          </select>
+          Position:{" "}
+          <input
+            value={colorBy.colorByPosition}
+            onChange={(e) => colorBy.setColorByPosition(e.target.value)}
+            type="number"
+            min="0"
+            className="inline-block w-56 bg-gray-100 text-sm mx-auto p-1 rounded border-gray-300 border m-1 text-gray-700"
+          />
+        </div>
+      )}
     </div>
   );
 }
