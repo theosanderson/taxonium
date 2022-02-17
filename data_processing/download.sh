db06757a32c8d859ece96afc338fb5372f707fc6
Theo Sanderson <theo@sndrsn.co.uk>
2022-02-17 01:48:52 +0000

    Update download.sh

--- data_processing/download.sh
+++ data_processing/download.sh
@@ -1,2 +1,6 @@
 wget -q -N https://hgwdev.gi.ucsc.edu/~angie/UShER_SARS-CoV-2/public-latest.all.masked.pb.gz
 wget  -q -N https://hgwdev.gi.ucsc.edu/~angie/UShER_SARS-CoV-2/public-latest.metadata.tsv.gz
+# Remove a bad sample
+zcat public-latest.metadata.tsv.gz | grep -v OM322554 > public-latest.metadata.tsv
+rm public-latest.metadata.tsv.gz
+gzip public-latest.metadata.tsv
