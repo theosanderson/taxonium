8b438c9d236d5be17f053f99600c16ea9510f7bd
Theo Sanderson <theo@sndrsn.co.uk>
2022-04-07 17:08:10 +0100

    Fix the issue that required commenting out
    Former-commit-id: 8997b76c03f7b5d77932844bad6b930871a65b6c

--- src/hooks/useBackend.js
+++ src/hooks/useBackend.js
@@ -3,12 +3,12 @@ import useLocalBackend from "./useLocalBackend";
 
 function useBackend(backend_url, uploaded_data, proto){
     const serverBackend = useServerBackend(backend_url);
-    //const localBackend = useLocalBackend(uploaded_data, proto);
+    const localBackend = useLocalBackend(uploaded_data, proto);
     if (backend_url) {
         return serverBackend;
     }
     if (uploaded_data) {
-        //return localBackend;
+        return localBackend;
     }
     else {
         return null;
