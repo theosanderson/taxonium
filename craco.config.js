2e39d6a4d608f9f12659fe65510ee10df9fb72d5
Mona Lisa <theo@theo.io>
2022-04-19 12:30:08 +0000

    Add GISAID headers to craco config

--- craco.config.js
+++ craco.config.js
@@ -8,4 +8,12 @@ module.exports = {
       ],
     },
   },
+  devServer: {
+    headers: {
+      "Access-Control-Allow-Origin": "https://platform.epicov.org",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+      "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization",
+      "Access-Control-Allow-Credentials": "true"
+    },
+  }
 }
