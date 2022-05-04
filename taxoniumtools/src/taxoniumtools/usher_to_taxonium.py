bc4c6c9f346cc4aa8d9da87dadf6fcec1ec90885
Theo Sanderson <theo@sndrsn.co.uk>
2022-05-04 15:58:29 +0100

    Refactor taxtools (#158)
    * Refactor
    
    * Automated autoyapf fixes
    
    * Cleanup
    
    * Automated autoyapf fixes
    
    * kw->pos
    
    * fix entry
    
    * Automated autoyapf fixes
    
    * fix
    
    * Automated autoyapf fixes
    
    * fix missing config
    
    * Automated autoyapf fixes
    
    Co-authored-by: github-actions <github-actions@github.com>

--- taxoniumtools/src/taxoniumtools/usher_to_taxonium.py
+++ taxoniumtools/src/taxoniumtools/usher_to_taxonium.py
@@ -3,20 +3,107 @@
 import json
 import pandas as pd
 from alive_progress import config_handler, alive_it, alive_bar
-import treeswift
-import tempfile
+
 from . import ushertools
+from . import utils
 import argparse
 import gzip
-import os
-import sys
-import warnings
 
 try:
     from . import _version
     version = _version.version
 except ImportError:
     version = "dev"
+
+
+def do_processing(input_file,
+                  output_file,
+                  metadata_file=None,
+                  genbank_file=None,
+                  columns=None,
+                  chronumental_enabled=False,
+                  chronumental_steps=100,
+                  chronumental_date_output=None,
+                  chronumental_reference_node=None,
+                  config_file=None):
+
+    metadata_dict, metadata_cols = utils.read_metadata(metadata_file, columns)
+
+    if config_file is not None:
+        config = json.load(open(config_file))
+    else:
+        config = {}
+
+    if "gz" in input_file:
+        f = gzip.open(input_file, 'rb')
+    else:
+        f = open(input_file, 'rb')
+
+    mat = ushertools.UsherMutationAnnotatedTree(f, genbank_file)
+    f.close()
+
+    if chronumental_enabled:
+        utils.do_chronumental(
+            mat=mat,
+            chronumental_reference_node=chronumental_reference_node,
+            metadata_file=metadata_file,
+            chronumental_steps=chronumental_steps,
+            chronumental_date_output=chronumental_date_output)
+
+    print("Ladderizing tree..")
+    mat.tree.ladderize(ascending=False)
+    print("Ladderizing done")
+    utils.set_x_coords(mat.tree.root,
+                       chronumental_enabled=chronumental_enabled)
+    utils.set_terminal_y_coords(mat.tree.root)
+    utils.set_internal_y_coords(mat.tree.root)
+
+    nodes_sorted_by_y = utils.sort_on_y(mat)
+    all_aa_muts_tuples = utils.get_all_aa_muts(mat.tree.root)
+    all_nuc_muts = utils.get_all_nuc_muts(mat.tree.root)
+    all_mut_inputs = all_aa_muts_tuples + all_nuc_muts
+    all_mut_objects = [
+        utils.make_aa_object(i, input_thing) if isinstance(input_thing, tuple)
+        else utils.make_nuc_object(i, input_thing)
+        for i, input_thing in enumerate(all_mut_inputs)
+    ]
+
+    input_to_index = {
+        input_thing: i
+        for i, input_thing in enumerate(all_mut_inputs)
+    }
+
+    first_json = {
+        "version": version,
+        "mutations": all_mut_objects,
+        "total_nodes": len(nodes_sorted_by_y),
+        "config": config
+    }
+
+    node_to_index = {node: i for i, node in enumerate(nodes_sorted_by_y)}
+
+    if "gz" in output_file:
+        output_file = gzip.open(output_file, 'wt')
+    else:
+        output_file = open(output_file, 'wt')
+    output_file.write(json.dumps(first_json, separators=(',', ':')) + "\n")
+    for node in alive_it(
+            nodes_sorted_by_y,
+            title="Converting each node, and writing out in JSON"):
+        node_object = utils.get_node_object(
+            node,
+            node_to_index,
+            metadata_dict,
+            input_to_index,
+            metadata_cols,
+            chronumental_enabled=chronumental_enabled)
+        output_file.write(
+            json.dumps(node_object, separators=(',', ':')) + "\n")
+    output_file.close()
+
+    print(
+        f"Done. Output written to {output_file}, with {len(nodes_sorted_by_y)} nodes."
+    )
 
 
 def main():
@@ -44,265 +131,31 @@ def main():
     parser.add_argument("--columns",
                         type=str,
                         help="Columns to include in the metadata")
-
-    parser.add_argument("--config_json",
-                        type=str,
-                        help="A JSON file to use as a config file",
-                        default=None)
-
-    parser.add_argument("--taxonium_date_file_output",
-                        type=str,
-                        help="Output file for the taxonium date file, if any")
+    parser.add_argument(
+        "--chronumental_date_output",
+        type=str,
+        help="Output file for the chronumental date file, if any")
 
     parser.add_argument("--chronumental_reference_node",
                         type=str,
                         help="Taxonium reference node",
                         default=None)
-
-    parser.add_argument('--gzlevel', type=int, help='Gzip level', default=6)
+    parser.add_argument("--config_json",
+                        type=str,
+                        help="A JSON file to use as a config file",
+                        default=None)
 
     args = parser.parse_args()
-
-    if args.config_json is not None:
-        config = json.load(open(args.config_json))
-    else:
-        config = {}
-
-    must_have_cols = ['strain']
-    cols_of_interest = set(args.columns.split(",")) if args.columns else set()
-    cols_of_interest.update(must_have_cols)
-    cols_of_interest = list(cols_of_interest)
-
-    #only load these cols:
-    if args.metadata:
-        print("Loading metadata file..")
-        #Disable warnings because of DTypeWarning in pandas
-
-        warnings.filterwarnings("ignore")
-        metadata = pd.read_csv(args.metadata,
-                               sep="\t" if args.metadata.endswith(".tsv")
-                               or args.metadata.endswith(".tsv.gz") else ",",
-                               usecols=cols_of_interest)
-        # Enable again
-        warnings.filterwarnings("default")
-        metadata.set_index("strain", inplace=True)
-        # convert metadata to dict of rows
-
-        metadata_dict = metadata.to_dict("index")
-        metadata_cols = metadata.columns
-        del metadata
-        print("Metadata loaded")
-    else:
-        metadata_dict = {}
-        metadata_cols = []
-
-    #config_handler.set_global(force_tty=True) # Uncommenting this will force progress bars
-
-    if "gz" in args.input:
-        f = gzip.open(args.input, 'rb')
-    else:
-        f = open(args.input, 'rb')
-
-    mat = ushertools.UsherMutationAnnotatedTree(f, args.genbank)
-    f.close()
-
-    if args.chronumental:
-        chronumental_is_available = os.system(
-            "which chronumental > /dev/null") == 0
-        if not chronumental_is_available:
-            print("#####  Chronumental is not available.  #####")
-            print(
-                "#####  Please install it with `pip install chronumental` and restart. Or you can disable the --chronumental flag.  #####"
-            )
-            print("#####  Exiting.  #####")
-            sys.exit(1)
-        with tempfile.TemporaryDirectory() as tmpdirname:
-            mat.tree.write_tree_newick(
-                os.path.join(tmpdirname, "distance_tree.nwk"))
-
-            print("Launching chronumental")
-
-            command = f"chronumental --tree {os.path.join(tmpdirname, 'distance_tree.nwk')} --dates {args.metadata} --steps {args.chronumental_steps} --tree_out {os.path.join(tmpdirname, 'timetree.nwk')}"
-            if args.chronumental_reference_node:
-                command += f" --reference_node {args.chronumental_reference_node}"
-
-            if args.taxonium_date_file_output:
-                command += f" --dates_out {args.taxonium_date_file_output}"
-            result = os.system(command)
-            if result != 0:
-                print("#####  Chronumental failed.  #####")
-                print("#####  Exiting.  #####")
-                sys.exit(1)
-
-            # %%
-
-            print("Reading time tree")
-            time_tree = treeswift.read_tree(os.path.join(
-                tmpdirname, "timetree.nwk"),
-                                            schema="newick")
-            time_tree_iter = ushertools.preorder_traversal(time_tree.root)
-            for i, node in alive_it(enumerate(
-                    ushertools.preorder_traversal(mat.tree.root)),
-                                    title="Adding time tree"):
-                time_tree_node = next(time_tree_iter)
-                if args.chronumental:
-                    node.time_length = time_tree_node.edge_length
-            del time_tree
-            del time_tree_iter
-
-    def set_x_coords(root):
-        """ Set x coordinates for the tree"""
-        root.x_dist = 0
-        root.x_time = 0
-        for node in alive_it(root.traverse_preorder(),
-                             title="Setting x coordinates"):
-            if node.parent:
-                node.x_dist = node.parent.x_dist + node.edge_length
-                if args.chronumental:
-                    node.x_time = node.parent.x_time + node.time_length
-
-    def set_terminal_y_coords(root):
-        for i, node in alive_it(enumerate(root.traverse_leaves()),
-                                title="Setting terminal y coordinates"):
-            node.y = i
-            node.y = i
-
-    def set_internal_y_coords(root):
-        # Each node should be halfway between the min and max y of its children
-        for node in alive_it(root.traverse_postorder(leaves=False,
-                                                     internal=True),
-                             title="Setting internal y coordinates"):
-
-            child_ys = [child.y for child in node.children]
-            node.y = (min(child_ys) + max(child_ys)) / 2
-
-    def get_all_aa_muts(root):
-        all_aa_muts = set()
-        for node in alive_it(list(root.traverse_preorder()),
-                             title="Collecting all AA mutations"):
-            if node.aa_muts:
-                all_aa_muts.update(node.aa_muts)
-        return list(all_aa_muts)
-
-    def get_all_nuc_muts(root):
-        all_nuc_muts = set()
-        for node in alive_it(list(root.traverse_preorder()),
-                             title="Collecting all nuc mutations"):
-            if node.nuc_mutations:
-                all_nuc_muts.update(node.nuc_mutations)
-        return list(all_nuc_muts)
-
-    def make_aa_object(i, aa_tuple):
-        # Tuple format is gene, position, prev, next
-        gene, pos, prev, next = aa_tuple
-        return {
-            "gene": gene,
-            "previous_residue": prev,
-            "residue_pos": pos,
-            "new_residue": next,
-            "mutation_id": i,
-            "type": "aa"
-        }
-
-    def make_nuc_object(i, nuc_mut):
-        return {
-            "gene": "nt",
-            "previous_residue": nuc_mut.par_nuc,
-            "residue_pos": nuc_mut.one_indexed_position,
-            "new_residue": nuc_mut.mut_nuc,
-            "mutation_id": i,
-            "type": "nt"
-        }
-
-    def get_node_object(node, node_to_index, metadata, input_to_index,
-                        columns):
-
-        object = {}
-        object["name"] = node.label if node.label else ""
-        # round to 5 dp
-        object["x_dist"] = round(node.x_dist, 5)
-        if args.chronumental:
-            object["x_time"] = round(node.x_time, 5)
-        object["y"] = node.y
-        object['mutations'] = [
-            input_to_index[my_input] for my_input in node.aa_muts
-        ] + [input_to_index[my_input] for my_input in node.nuc_mutations]
-        # check if label is in metadata's index
-        try:
-            my_dict = metadata[node.label]
-            for key in my_dict:
-                value = my_dict[key]
-                #if value is pd.NaN then set to empty string
-                if pd.isna(value):
-                    value = ""
-                object["meta_" + key] = value
-        except KeyError:
-            for key in columns:
-                object["meta_" + key] = ""
-
-        object['parent_id'] = node_to_index[
-            node.parent] if node.parent else node_to_index[node]
-        object['node_id'] = node_to_index[
-            node]  # We don't strictly need this, but it doesn't add much to the space
-
-        object['num_tips'] = node.num_tips
-        return object
-
-    print("Ladderizing tree..")
-    mat.tree.ladderize(ascending=False)
-    print("Ladderizing done")
-    set_x_coords(mat.tree.root)
-    set_terminal_y_coords(mat.tree.root)
-    set_internal_y_coords(mat.tree.root)
-
-    with alive_bar(title="Sorting on y") as bar:
-
-        def return_y(node):
-            bar()
-            return node.y
-
-        nodes_sorted_by_y = sorted(mat.tree.root.traverse_preorder(),
-                                   key=lambda x: return_y(x))
-
-    all_aa_muts_tuples = get_all_aa_muts(mat.tree.root)
-    all_nuc_muts = get_all_nuc_muts(mat.tree.root)
-    all_mut_inputs = all_aa_muts_tuples + all_nuc_muts
-    all_mut_objects = [
-        make_aa_object(i, input_thing)
-        if isinstance(input_thing, tuple) else make_nuc_object(i, input_thing)
-        for i, input_thing in enumerate(all_mut_inputs)
-    ]
-    input_to_index = {
-        input_thing: i
-        for i, input_thing in enumerate(all_mut_inputs)
-    }
-
-    first_json = {
-        "version": version,
-        "mutations": all_mut_objects,
-        "total_nodes": len(nodes_sorted_by_y),
-        "config": config
-    }
-
-    node_to_index = {node: i for i, node in enumerate(nodes_sorted_by_y)}
-
-    if "gz" in args.output:
-        output_file = gzip.open(args.output, 'wt', compresslevel=args.gzlevel)
-    else:
-        output_file = open(args.output, 'wt')
-    output_file.write(json.dumps(first_json, separators=(',', ':')) + "\n")
-    for node in alive_it(
-            nodes_sorted_by_y,
-            title="Converting each node, and writing out in JSON"):
-        node_object = get_node_object(node, node_to_index, metadata_dict,
-                                      input_to_index, metadata_cols)
-        output_file.write(
-            json.dumps(node_object, separators=(',', ':')) + "\n")
-    output_file.close()
-
-    print(
-        f"Done. Output written to {args.output}, with {len(nodes_sorted_by_y)} nodes."
-    )
+    do_processing(args.input,
+                  args.output,
+                  metadata_file=args.metadata,
+                  genbank_file=args.genbank,
+                  chronumental_enabled=args.chronumental,
+                  chronumental_steps=args.chronumental_steps,
+                  columns=args.columns,
+                  chronumental_date_output=args.chronumental_date_output,
+                  chronumental_reference_node=args.chronumental_reference_node,
+                  config_file=args.config_json)
 
 
 if __name__ == "__main__":
---
+++ taxoniumtools/src/taxoniumtools/utils.py
@@ -0,0 +1,201 @@
+from alive_progress import alive_it, alive_bar
+import pandas as pd
+import warnings
+import os, tempfile, sys
+import treeswift
+from . import ushertools
+
+
+def read_metadata(metadata_file, columns):
+    must_have_cols = ['strain']
+    cols_of_interest = set(columns.split(",")) if columns else set()
+    cols_of_interest.update(must_have_cols)
+    cols_of_interest = list(cols_of_interest)
+
+    #only load these cols:
+    if metadata_file:
+        print("Loading metadata file..")
+        #Disable warnings because of DTypeWarning in pandas
+
+        warnings.filterwarnings("ignore")
+        metadata = pd.read_csv(metadata_file,
+                               sep="\t" if metadata_file.endswith(".tsv")
+                               or metadata_file.endswith(".tsv.gz") else ",",
+                               usecols=cols_of_interest)
+        # Enable again
+        warnings.filterwarnings("default")
+        metadata.set_index("strain", inplace=True)
+        # convert metadata to dict of rows
+
+        metadata_dict = metadata.to_dict("index")
+        metadata_cols = metadata.columns
+        del metadata
+        print("Metadata loaded")
+        return metadata_dict, metadata_cols
+    else:
+        metadata_dict = {}
+        metadata_cols = []
+        return metadata_dict, metadata_cols
+
+
+def do_chronumental(mat, chronumental_reference_node, metadata_file,
+                    chronumental_steps, chronumental_date_output):
+    chronumental_is_available = os.system(
+        "which chronumental > /dev/null") == 0
+    if not chronumental_is_available:
+        print("#####  Chronumental is not available.  #####")
+        print(
+            "#####  Please install it with `pip install chronumental` and restart. Or you can disable the --chronumental flag.  #####"
+        )
+        print("#####  Exiting.  #####")
+        sys.exit(1)
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        mat.tree.write_tree_newick(
+            os.path.join(tmpdirname, "distance_tree.nwk"))
+
+        print("Launching chronumental")
+
+        command = f"chronumental --tree {os.path.join(tmpdirname, 'distance_tree.nwk')} --dates {metadata_file} --steps {chronumental_steps} --tree_out {os.path.join(tmpdirname, 'timetree.nwk')}"
+        if chronumental_reference_node:
+            command += f" --reference_node {chronumental_reference_node}"
+
+        if chronumental_date_output:
+            command += f" --dates_out {chronumental_date_output}"
+        result = os.system(command)
+        if result != 0:
+            print("#####  Chronumental failed.  #####")
+            print("#####  Exiting.  #####")
+            sys.exit(1)
+
+        # %%
+
+        print("Reading time tree")
+        time_tree = treeswift.read_tree(os.path.join(tmpdirname,
+                                                     "timetree.nwk"),
+                                        schema="newick")
+        time_tree_iter = ushertools.preorder_traversal(time_tree.root)
+        for i, node in alive_it(enumerate(
+                ushertools.preorder_traversal(mat.tree.root)),
+                                title="Adding time tree"):
+            time_tree_node = next(time_tree_iter)
+
+            node.time_length = time_tree_node.edge_length
+        del time_tree
+        del time_tree_iter
+
+
+def set_x_coords(root, chronumental_enabled):
+    """ Set x coordinates for the tree"""
+    root.x_dist = 0
+    root.x_time = 0
+    for node in alive_it(root.traverse_preorder(),
+                         title="Setting x coordinates"):
+        if node.parent:
+            node.x_dist = node.parent.x_dist + node.edge_length
+            if chronumental_enabled:
+                node.x_time = node.parent.x_time + node.time_length
+
+
+def set_terminal_y_coords(root):
+    for i, node in alive_it(enumerate(root.traverse_leaves()),
+                            title="Setting terminal y coordinates"):
+        node.y = i
+        node.y = i
+
+
+def set_internal_y_coords(root):
+    # Each node should be halfway between the min and max y of its children
+    for node in alive_it(root.traverse_postorder(leaves=False, internal=True),
+                         title="Setting internal y coordinates"):
+
+        child_ys = [child.y for child in node.children]
+        node.y = (min(child_ys) + max(child_ys)) / 2
+
+
+def get_all_aa_muts(root):
+    all_aa_muts = set()
+    for node in alive_it(list(root.traverse_preorder()),
+                         title="Collecting all AA mutations"):
+        if node.aa_muts:
+            all_aa_muts.update(node.aa_muts)
+    return list(all_aa_muts)
+
+
+def get_all_nuc_muts(root):
+    all_nuc_muts = set()
+    for node in alive_it(list(root.traverse_preorder()),
+                         title="Collecting all nuc mutations"):
+        if node.nuc_mutations:
+            all_nuc_muts.update(node.nuc_mutations)
+    return list(all_nuc_muts)
+
+
+def make_aa_object(i, aa_tuple):
+    # Tuple format is gene, position, prev, next
+    gene, pos, prev, next = aa_tuple
+    return {
+        "gene": gene,
+        "previous_residue": prev,
+        "residue_pos": pos,
+        "new_residue": next,
+        "mutation_id": i,
+        "type": "aa"
+    }
+
+
+def make_nuc_object(i, nuc_mut):
+    return {
+        "gene": "nt",
+        "previous_residue": nuc_mut.par_nuc,
+        "residue_pos": nuc_mut.one_indexed_position,
+        "new_residue": nuc_mut.mut_nuc,
+        "mutation_id": i,
+        "type": "nt"
+    }
+
+
+def get_node_object(node, node_to_index, metadata, input_to_index, columns,
+                    chronumental_enabled):
+
+    object = {}
+    object["name"] = node.label if node.label else ""
+    # round to 5 dp
+    object["x_dist"] = round(node.x_dist, 5)
+    if chronumental_enabled:
+        object["x_time"] = round(node.x_time, 5)
+    object["y"] = node.y
+    object['mutations'] = [
+        input_to_index[my_input] for my_input in node.aa_muts
+    ] + [input_to_index[my_input] for my_input in node.nuc_mutations]
+    # check if label is in metadata's index
+    try:
+        my_dict = metadata[node.label]
+        for key in my_dict:
+            value = my_dict[key]
+            #if value is pd.NaN then set to empty string
+            if pd.isna(value):
+                value = ""
+            object["meta_" + key] = value
+    except KeyError:
+        for key in columns:
+            object["meta_" + key] = ""
+
+    object['parent_id'] = node_to_index[
+        node.parent] if node.parent else node_to_index[node]
+    object['node_id'] = node_to_index[
+        node]  # We don't strictly need this, but it doesn't add much to the space
+
+    object['num_tips'] = node.num_tips
+    return object
+
+
+def sort_on_y(mat):
+    with alive_bar(title="Sorting on y") as bar:
+
+        def return_y(node):
+            bar()
+            return node.y
+
+        nodes_sorted_by_y = sorted(mat.tree.root.traverse_preorder(),
+                                   key=lambda x: return_y(x))
+    return nodes_sorted_by_y
