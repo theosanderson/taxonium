573882ec2b768b5076f53c798d0348e669ce3618
Theo Sanderson <theo@sndrsn.co.uk>
2022-05-06 13:00:56 +0000

    update

--- taxonium_web_client/src/components/SearchTopLayerItem.jsx
+++ taxonium_web_client/src/components/SearchTopLayerItem.jsx
@@ -91,7 +91,7 @@ function SearchTopLayerItem({ singleSearchSpec, myKey, search, config }) {
               >
                 <FaSearch />
               </button>{" "}{ // check if window href includes 'protoUrl'
-                window.location.href.includes("protoUrl") && (
+                (window.location.href.includes("protoUrl") || window.location.href.includes("backend")) && (
                
               <button
                 className="inline-block bg-gray-100 text-xs mx-auto h-5 rounded border-gray-300 border m-4 text-gray-700"
