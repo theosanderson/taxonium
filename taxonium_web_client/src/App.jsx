d0bcee06a9aa4a6f419d5a68b6413c20dec6156a
Theo Sanderson <theo@sndrsn.co.uk>
2022-05-11 14:39:52 +0100

    Update App.jsx

--- taxonium_web_client/src/App.jsx
+++ taxonium_web_client/src/App.jsx
@@ -270,7 +270,6 @@ function App() {
                 >
                   full-download
                 </a> (slower to start) version
-                .
               </p>{" "}
             </div>
           )}
