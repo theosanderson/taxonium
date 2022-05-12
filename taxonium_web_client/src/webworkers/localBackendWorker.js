fd4f9e12248d31d55f76d202b6f56deea8aaec7a
Theo Sanderson <theo@sndrsn.co.uk>
2022-05-12 17:19:56 +0000

    fix issues

--- taxonium_web_client/src/webworkers/localBackendWorker.js
+++ taxonium_web_client/src/webworkers/localBackendWorker.js
@@ -238,9 +238,9 @@ const getDetails = async (node_id) => {
   const node = nodes[node_id];
   console.log("node is ", node);
   const details = { ...node };
-  details.mutations = processedUploadedData.node_to_mut[node_id].map(
-    (x) => processedUploadedData.mutations[x]
-  );
+  details.mutations = processedUploadedData.node_to_mut[node_id] ? processedUploadedData.node_to_mut[node_id].map(
+    (x) => processedUploadedData.mutations[x] 
+  ): [];
   console.log("details is ", details);
   return details;
 };
