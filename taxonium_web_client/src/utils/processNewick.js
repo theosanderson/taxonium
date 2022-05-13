3c76a4732ce8fd6a56ebb40141baa136c475de1f
Theo Sanderson <theo@sndrsn.co.uk>
2022-05-13 01:00:43 +0100

    Update processNewick.js

--- taxonium_web_client/src/utils/processNewick.js
+++ taxonium_web_client/src/utils/processNewick.js
@@ -76,6 +76,8 @@ async function cleanup(tree) {
   });
 
   tree.node.forEach((node) => {
+    if node.name:
+      node.name = node.name.replace("'","");
     node.parent_id = node.parent ? node.parent.node_id : node.node_id;
     delete node.parent;
     node.x_dist = node.x;
