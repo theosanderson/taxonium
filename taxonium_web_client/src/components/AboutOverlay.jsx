f2a1aef30d53dfb593e3609eb05c0a7477b9aa4b
Theo Sanderson <theo@sndrsn.co.uk>
2022-05-17 20:26:40 +0100

    update

--- taxonium_web_client/src/components/AboutOverlay.jsx
+++ taxonium_web_client/src/components/AboutOverlay.jsx
@@ -5,7 +5,21 @@ function AboutOverlay({ enabled, setEnabled, overlayContent }) {
   return (
     <Modal isOpen={enabled} onRequestClose={() => setEnabled(false)}>
       <div dangerouslySetInnerHTML={{ __html: overlayContent }} />
-      {!overlayContent && <div></div>}
+      {!overlayContent &&  <div className="m-5">
+              <h2 className="font-bold mb-3">Welcome to Taxonium</h2>
+              <p className="mb-1">
+                Taxonium is a web application for visualizing phylogenetic
+                trees. Find out more at our{" "}
+                <a
+                  className="text-blue underline"
+                  href="https://github.com/theosanderson/taxodium"
+                >
+                  Github repository
+                </a>
+                .
+              </p>
+              <p>Taxonium was created by Theo Sanderson.</p>
+            </div>}
     </Modal>
   );
 }
