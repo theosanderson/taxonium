6b6d876795ef721d67ad466083aa0ec46573de97
Theo Sanderson <theo@sndrsn.co.uk>
2022-05-17 15:19:18 +0000

    Remove styling

--- taxonium_web_client/src/components/AboutOverlay.jsx
+++ taxonium_web_client/src/components/AboutOverlay.jsx
@@ -1,25 +1,14 @@
 import React, { useRef, useEffect } from "react";
 import Modal from "react-modal";
 
-const settingsModalStyle = {
-  content: {
-    top: "50%",
-    left: "50%",
-    right: "auto",
-    bottom: "auto",
-    marginRight: "-50%",
-    transform: "translate(-50%, -50%)",
-    //width: '50%',
-    backgroundColor: "#fafafa",
-  },
-};
+
 
 function AboutOverlay({ enabled, setEnabled, overlayContent }) {
 
   return (
     <Modal
       isOpen={enabled}
-      style={settingsModalStyle}
+   
       onRequestClose={() => setEnabled(false)}
     >
       <div dangerouslySetInnerHTML={{ __html: overlayContent }} />
