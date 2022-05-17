fdc179d8d4f7e12987e7025263bf1abf02d873d5
theosanderson <theosanderson@users.noreply.github.com>
2022-05-17 14:20:05 +0000

    Prettified Code!

--- taxonium_web_client/src/components/AboutOverlay.jsx
+++ taxonium_web_client/src/components/AboutOverlay.jsx
@@ -14,31 +14,29 @@ const settingsModalStyle = {
   },
 };
 
-
 function AboutOverlay({ enabled, setEnabled, overlayRef }) {
   return (
-    <Modal isOpen={enabled} style={settingsModalStyle} onRequestClose={() => setEnabled(false)} >
-      
-    
-          
-
-          <div ref={overlayRef}>
-            <div className="m-5">
-              <h2 className="font-bold mb-3">Welcome to Taxonium</h2>
-              <p className="mb-1">
-                Taxonium is a web application for visualizing phylogenetic
-                trees. Find out more at our{" "}
-                <a
-                  className="text-blue underline"
-                  href="https://github.com/theosanderson/taxodium"
-                >
-                  Github repository
-                </a>
-                .
-              </p>
-              <p>Taxonium was created by Theo Sanderson.</p>
-            </div>
-         
+    <Modal
+      isOpen={enabled}
+      style={settingsModalStyle}
+      onRequestClose={() => setEnabled(false)}
+    >
+      <div ref={overlayRef}>
+        <div className="m-5">
+          <h2 className="font-bold mb-3">Welcome to Taxonium</h2>
+          <p className="mb-1">
+            Taxonium is a web application for visualizing phylogenetic trees.
+            Find out more at our{" "}
+            <a
+              className="text-blue underline"
+              href="https://github.com/theosanderson/taxodium"
+            >
+              Github repository
+            </a>
+            .
+          </p>
+          <p>Taxonium was created by Theo Sanderson.</p>
+        </div>
       </div>
     </Modal>
   );
