7d7a20b228481bf2a06345dfb205175ce2cb7f87
Theo Sanderson <theo@sndrsn.co.uk>
2022-05-17 16:31:09 +0100

    Formatting (#196)
    * update
    
    * Prettified Code!
    
    Co-authored-by: theosanderson <theosanderson@users.noreply.github.com>

--- taxonium_web_client/src/components/AboutOverlay.jsx
+++ taxonium_web_client/src/components/AboutOverlay.jsx
@@ -1,21 +1,11 @@
 import React, { useRef, useEffect } from "react";
 import Modal from "react-modal";
 
-
-
 function AboutOverlay({ enabled, setEnabled, overlayContent }) {
-
   return (
-    <Modal
-      isOpen={enabled}
-   
-      onRequestClose={() => setEnabled(false)}
-    >
+    <Modal isOpen={enabled} onRequestClose={() => setEnabled(false)}>
       <div dangerouslySetInnerHTML={{ __html: overlayContent }} />
-       {!overlayContent && 
-        <div>
-        
-      </div>}
+      {!overlayContent && <div></div>}
     </Modal>
   );
 }
