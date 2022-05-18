370cbd4bac9fc2f248e6430cc61949b046c22c18
Theo Sanderson <theo@sndrsn.co.uk>
2022-05-18 19:17:38 +0000

    update

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,6 @@ sys.path.insert(0, os.path.abspath('../'))
 # -- Project information -----------------------------------------------------
 
 project = 'Taxonium'
-copyright = '2022, Theo Sanderson'
 author = 'Theo Sanderson'
 
 # The full version, including alpha/beta/rc tags
