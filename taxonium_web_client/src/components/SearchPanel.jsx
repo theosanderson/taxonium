876dd29624c8b1d221e05952f20b69d9c23683d4
Theo Sanderson <theo@sndrsn.co.uk>
2022-05-19 23:02:05 +0000

    updates

--- taxonium_web_client/src/components/SearchPanel.jsx
+++ taxonium_web_client/src/components/SearchPanel.jsx
@@ -38,6 +38,9 @@ function SearchPanel({
     if (config && config.customNames && config.customNames[name]) {
       return config.customNames[name];
     }
+    if (name === "num_tips") {
+      return "Number of descendants";
+    }
     const new_name = name.replace("meta_", "").replace("_", " ");
     return new_name.charAt(0).toUpperCase() + new_name.slice(1);
   };
