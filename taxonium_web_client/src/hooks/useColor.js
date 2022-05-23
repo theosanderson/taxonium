5d8378f46e0662f1977990da73510313b5eca96f
Theo Sanderson <theo@sndrsn.co.uk>
2022-05-23 21:41:17 +0100

    Update useColor.js

--- taxonium_web_client/src/hooks/useColor.js
+++ taxonium_web_client/src/hooks/useColor.js
@@ -103,6 +103,11 @@ const useColor = (colorMapping) => {
       if (string === "Denmark") {
         return [24, 112, 32]; // diff to UK
       }
+      
+      if (string === "Democratic Republic of the Congo") {
+        return [17, 58, 99]; // otherwise too similar to CAR
+      }
+      
 
       string = string.split("").reverse().join("");
       var hash = 0;
