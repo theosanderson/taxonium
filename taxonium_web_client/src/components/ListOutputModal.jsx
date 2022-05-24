14ccc0ec4c943a0aade32d9ea42d5aa43153398b
Theo Sanderson <theo@sndrsn.co.uk>
2022-05-24 19:04:15 +0100

    update

--- taxonium_web_client/src/components/ListOutputModal.jsx
+++ taxonium_web_client/src/components/ListOutputModal.jsx
@@ -90,7 +90,7 @@ const ListOutputModal = ({
         )}
         {!loading && listOutput.length > 0 && (
           <textarea
-            className="w-full h-64 bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
+            className="w-full h-64 bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 shadow leading-tight focus:outline-none focus:shadow-outline"
             value={listOutput.join("\n")}
             readOnly
           />
