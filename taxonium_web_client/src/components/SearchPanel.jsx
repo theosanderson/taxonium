51bb3fac27f1d165382f0e96a6a633bc62f0df61
Theo Sanderson <theo@sndrsn.co.uk>
2022-05-26 18:06:55 +0100

    update

--- taxonium_web_client/src/components/SearchPanel.jsx
+++ taxonium_web_client/src/components/SearchPanel.jsx
@@ -134,6 +134,7 @@ function SearchPanel({
 
   return (
     <div className="overflow-y-auto" style={{ height: "calc(100vh - 5em)" }}>
+      <FirefoxWarning className="text-xs font-bold text-gray-700" />
       <div className="mt-3 mb-3 text-gray-500 text-sm">
         {config.num_tips && (
           <>
@@ -143,7 +144,7 @@ function SearchPanel({
           </>
         )}
       </div>
-      <FirefoxWarning className="text-xs font-bold" />
+      
       {config.x_accessors && config.x_accessors.length > 1 && (
         <div className="border-t md:border-t-0 border-b border-gray-300 pb-2 mb-2 text-gray-500">
           Tree type:{" "}
