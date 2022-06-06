2ea3ec82002bcc11665462e56dac205c67e91d81
Theo Sanderson <theo@sndrsn.co.uk>
2022-06-06 13:09:46 +0100

    Update Basic.jsx

--- taxonium_web_client/src/components/Basic.jsx
+++ taxonium_web_client/src/components/Basic.jsx
@@ -20,7 +20,7 @@ export const Select = ({ onChange, className, children, value, title }) => {
   return (
     <select
       className={classNames(
-        "border bg-white text-gray-900 text-sm hover:text-gray-700 py-1 pl-2",
+        "border bg-white text-gray-900 text-sm hover:text-gray-700 py-1 pl-2 pr-2",
         "focus:ring-gray-800 focus:border-gray-800",
         className
       )}
