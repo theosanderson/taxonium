b8917ea89787ce8cd924fbfd668748f05cce5874
Theo Sanderson <theo@sndrsn.co.uk>
2022-06-06 01:17:20 +0100

    update

--- taxonium_web_client/tailwind.config.js
+++ taxonium_web_client/tailwind.config.js
@@ -3,5 +3,17 @@ module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   plugins: [
     require('@tailwindcss/forms')
-  ]
+  ],
+  theme: {
+    customForms: theme => ({
+      default: {
+        input: {
+       
+          borderColor: theme('colors.gray.200'),
+        
+        },
+    
+      },
+    })
+  },
 };
