d50a66498e3b091efaaeebfa6c7b2bc5fcec0698
Theo Sanderson <theo@sndrsn.co.uk>
2022-07-19 15:27:04 +0100

    More

--- taxonium_web_client/src/utils/processNewick.js
+++ taxonium_web_client/src/utils/processNewick.js
@@ -59,6 +59,23 @@ async function do_fetch(url, sendStatusMessage, whatIsBeingDownloaded) {
           percentage: (progress.loaded / progress.total) * 100,
         });
       },
+    }).catch(function (error) {
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        window.alert(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+      } else if (error.request) {
+        // The request was made but no response was received
+        // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+        // http.ClientRequest in node.js
+        console.log(error.request);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        window.alert("Error", error.message);
+      }
+      console.log(error.config);
     });
     const text = response.data;
     //parse text:
