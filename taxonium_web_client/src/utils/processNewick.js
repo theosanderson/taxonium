e98d4cd7dc7a842b28cb54c0fef22c7abb95b147
Theo Sanderson <theo@sndrsn.co.uk>
2022-11-22 14:44:00 +0000

    Update processNewick.js

--- taxonium_web_client/src/utils/processNewick.js
+++ taxonium_web_client/src/utils/processNewick.js
@@ -205,7 +205,7 @@ export async function processMetadataFile(data, sendStatusMessage) {
   const output = new Map();
   let splitFunction;
 
-  if (data.filename.includes(data.filetype == "meta_csv")) {
+  if (data.filetype == "meta_csv") {
     splitFunction = (x) => x.split("\t");
   } else if (data.filetype == "meta_csv") {
     // remove any double quotes
