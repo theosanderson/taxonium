013d1b0acccc6bc82d5980cfad5210111e95e387
Theo Sanderson <theo@theo.io>
2023-05-24 18:09:55 +0100

    test

--- taxonium_component/src/Taxonium.jsx
+++ taxonium_component/src/Taxonium.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import Deck from "./Deck";
-import SearchPanel from "./components/SearchPanel";
+import SearchPanel from "./components/SearchPanel.jsx";
 import useTreenomeState from "./hooks/useTreenomeState";
 import useView from "./hooks/useView";
 import useGetDynamicData from "./hooks/useGetDynamicData";
