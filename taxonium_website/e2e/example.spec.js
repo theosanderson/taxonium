990c152cc78d353280e32cd71390563f9cab1109
Theo Sanderson <theo@sndrsn.co.uk>
2023-10-12 21:12:48 +0100

    Test cont (#547)
    * a
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * up
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- taxonium_website/e2e/example.spec.js
+++ taxonium_website/e2e/example.spec.js
@@ -4,8 +4,6 @@ import { test, expect } from "@playwright/test";
 test("has title", async ({ page }) => {
   await page.goto("//localhost:4173/");
 
-
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Taxonium/);
 });
-
