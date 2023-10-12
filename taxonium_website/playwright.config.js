1064f4e6a27066da0d2d210ecc019d146cece5a1
Theo Sanderson <theo@theo.io>
2023-10-12 17:32:46 +0100

    ad

--- taxonium_website/playwright.config.js
+++ taxonium_website/playwright.config.js
@@ -14,12 +14,8 @@ export default defineConfig({
   testDir: "./e2e",
   /* Run tests in files in parallel */
   fullyParallel: true,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
-  forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
@@ -71,7 +67,6 @@ export default defineConfig({
 
   webServer: {
     command: "yarn preview",
-    reuseExistingServer: !process.env.CI,
     port: 4173,
   },
 });
