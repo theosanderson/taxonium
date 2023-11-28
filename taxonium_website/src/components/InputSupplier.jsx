2dc8d21c320949d0b5c8fcd77d41b5a8b228452d
Theo Sanderson <theo@theo.io>
2023-11-28 14:03:15 +0000

    update

--- taxonium_website/src/components/InputSupplier.jsx
+++ taxonium_website/src/components/InputSupplier.jsx
@@ -30,17 +30,25 @@ export const InputSupplier = ({ inputHelper, className }) => {
   });
 
   const [tempURL, setTempURL] = useState("");
+  const [useProxy, setUseProxy] = useState(true); // New state for proxy usage
+
 
   const { inputs, setInputs } = inputHelper;
   const [addingText, setAddingText] = useState(false);
   const [text, setText] = useState("");
 
+  
   const addFromTempURL = useCallback(() => {
-    if (tempURL) {
-      inputHelper.addFromURL(tempURL);
+    let finalURL = tempURL;
+    if (useProxy) {
+      finalURL = `https://proxy.taxonium.org/proxy?url=${encodeURIComponent(tempURL)}`;
+    }
+    if (finalURL) {
+      inputHelper.addFromURL(finalURL);
       setTempURL("");
     }
-  }, [tempURL, inputHelper]);
+  }, [tempURL, useProxy, inputHelper]); // Include useProxy in the dependency array
+
 
   return (
     <div className={className}>
@@ -196,6 +204,17 @@ export const InputSupplier = ({ inputHelper, className }) => {
                 }
               }}
             />{" "}
+            {
+              tempURL !== "" && <>
+          <input
+            type="checkbox"
+            checked={useProxy}
+            onChange={(e) => setUseProxy(e.target.checked)}
+          />{" "}
+          Use Proxy
+          </>
+}
+
             <Button onClick={addFromTempURL} className="">
               Add
             </Button>
