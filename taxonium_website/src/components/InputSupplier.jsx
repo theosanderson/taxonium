b05dec4734a1f009aa0ee01f42c0f10a8dc6a8cb
Theo Sanderson <theo@theo.io>
2023-11-28 14:36:46 +0000

    up

--- taxonium_website/src/components/InputSupplier.jsx
+++ taxonium_website/src/components/InputSupplier.jsx
@@ -202,15 +202,18 @@ export const InputSupplier = ({ inputHelper, className }) => {
             />{" "}
             {tempURL !== "" && (
               <>
+              <span className="mr-2 text-sm text-gray-600">
                 <input
                   type="checkbox"
                   checked={useProxy}
-                  className="mr-2 text-sm text-gray-600"
+                  className=""
                   onChange={(e) => setUseProxy(e.target.checked)}
                 />{" "}
                 Use Proxy
+                </span>
               </>
             )}
+            
             <Button onClick={addFromTempURL} className="">
               Add
             </Button>
