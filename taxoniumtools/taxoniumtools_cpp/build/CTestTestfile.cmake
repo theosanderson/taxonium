022ab4a8c5a56cb147616084bfefd7b065b39932
Theo Sanderson <theo@theo.io>
2025-07-01 16:25:56 +0100

    update

--- taxoniumtools/taxoniumtools_cpp/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/theosanderson/taxonium-11/taxoniumtools/taxoniumtools_cpp
-# Build directory: /Users/theosanderson/taxonium-11/taxoniumtools/taxoniumtools_cpp/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("test")
--- taxoniumtools/taxoniumtools_cpp/build/test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/theosanderson/taxonium-11/taxoniumtools/taxoniumtools_cpp/test
-# Build directory: /Users/theosanderson/taxonium-11/taxoniumtools/taxoniumtools_cpp/build/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(BasicTests "/Users/theosanderson/taxonium-11/taxoniumtools/taxoniumtools_cpp/build/test/test_taxonium")
-set_tests_properties(BasicTests PROPERTIES  _BACKTRACE_TRIPLES "/Users/theosanderson/taxonium-11/taxoniumtools/taxoniumtools_cpp/test/CMakeLists.txt;13;add_test;/Users/theosanderson/taxonium-11/taxoniumtools/taxoniumtools_cpp/test/CMakeLists.txt;0;")
