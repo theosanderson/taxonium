a2062496b9060472bf502fea3327f8aa0166c5c7
Theo Sanderson <theo@theo.io>
2025-10-16 22:28:27 +0100

    update

--- taxonium_website_next/src/app/[...slug]/page.tsx
+++ taxonium_website_next/src/app/[...slug]/page.tsx
@@ -1,9 +1,14 @@
 "use client";
 
+import { Suspense } from 'react';
 import { usePathname } from 'next/navigation';
 import MainApp from '../MainApp';
 
 export default function TreePage() {
   const pathname = usePathname();
-  return <MainApp pathname={pathname} />;
+  return (
+    <Suspense fallback={<div></div>}>
+      <MainApp pathname={pathname} />
+    </Suspense>
+  );
 }
